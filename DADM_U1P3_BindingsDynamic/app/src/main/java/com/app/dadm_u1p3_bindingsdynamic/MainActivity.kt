8048f3624ab04810da1ebf964f79e754e7d5e3b9
Panchis7u7 <smadrigal.rod@gmail.com>
2021-03-19 10:37:28 -0600

    Dynamic rendering.

---
+++ DADM_U1P3_BindingsDynamic/app/src/main/java/com/app/dadm_u1p3_bindingsdynamic/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.app.dadm_u1p3_bindingsdynamic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ DADM_U1P3_BindingsDynamic/app/src/test/java/com/app/dadm_u1p3_bindingsdynamic/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.app.dadm_u1p3_bindingsdynamic
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
