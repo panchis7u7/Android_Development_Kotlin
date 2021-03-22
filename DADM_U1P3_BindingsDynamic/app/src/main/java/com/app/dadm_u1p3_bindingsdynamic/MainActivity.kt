15b8224329a5c773299ab14e5cfed34732c070ae
panchis7u7 <smadrigal.rod@gmail.com>
2021-03-22 09:14:30 -0600

    Bindings.

--- DADM_U1P3_BindingsDynamic/app/src/main/java/com/app/dadm_u1p3_bindingsdynamic/MainActivity.kt
+++ DADM_U1P3_BindingsDynamic/app/src/main/java/com/app/dadm_u1p3_bindingsdynamic/MainActivity.kt
@@ -2,10 +2,22 @@ package com.app.dadm_u1p3_bindingsdynamic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.app.dadm_u1p3_bindingsdynamic.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var pelicula = ""
+    private var horario = ""
+    private var sala =""
+    private var nBoletos = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val salas
     }
 }
