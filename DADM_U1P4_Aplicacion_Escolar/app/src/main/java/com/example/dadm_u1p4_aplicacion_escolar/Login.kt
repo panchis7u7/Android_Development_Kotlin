13201f6c01803c26405eb867a473a5e296a1b87b
panchis7u7 <smadrigal.rod@gmail.com>
2021-03-30 16:00:01 -0600

    Autenticacion con firebase.

--- DADM_U1P4_Aplicacion_Escolar/app/src/main/java/com/example/dadm_u1p4_aplicacion_escolar/Login.kt
+++ DADM_U1P4_Aplicacion_Escolar/app/src/main/java/com/example/dadm_u1p4_aplicacion_escolar/Login.kt
@@ -3,8 +3,10 @@ package com.example.dadm_u1p4_aplicacion_escolar
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import com.example.dadm_u1p4_aplicacion_escolar.databinding.ActivityLoginBinding
-import com.squareup.picasso.Picasso
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity() {
 
@@ -15,9 +17,32 @@ class Login : AppCompatActivity() {
         //setContentView(R.layout.activity_login)
         setContentView(binding.root)
 
+        //Analytics.
+        val analytics: FirebaseAnalytics = FirebaseAnalytics.getInstance(this)
+        val bundle: Bundle = Bundle()
+        bundle.putString("message", "Integracion de firebase exitosa.")
+        analytics.logEvent("InitScreen", bundle)
+
         binding.btnLogin.setOnClickListener{
-            val intent = Intent(this, Dashboard::class.java)
-            startActivity(intent)
+            if(binding.textViewControl.text.isNotEmpty() && binding.textViewContra.text.isNotEmpty()){
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(
+                    binding.textViewControl.text.toString(),
+                    binding.textViewContra.text.toString()
+                ).addOnCompleteListener {
+                    if(it.isSuccessful){
+                        val intent = Intent(this, Dashboard::class.java)
+                        startActivity(intent)
+                    } else {
+                        val builder = AlertDialog.Builder(this)
+                        builder.setTitle("Error")
+                        builder.setMessage("Se ha producido un error con la autenticacion del usuario.")
+                        builder.setPositiveButton("Aceptar", null)
+                        val dialog: AlertDialog = builder.create()
+                        dialog.show()
+                    }
+
+                }
+            }
         }
     }
 }
