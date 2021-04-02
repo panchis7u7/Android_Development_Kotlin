305dbc2b0e8efbff21b55de0b56a77cacd65c336
panchis7u7 <smadrigal.rod@gmail.com>
2021-04-01 18:48:18 -0600

    Avance curricular.

--- DADM_U1P4_Aplicacion_Escolar/app/src/main/java/com/example/dadm_u1p4_aplicacion_escolar/AvanceCurricular.kt
+++ DADM_U1P4_Aplicacion_Escolar/app/src/main/java/com/example/dadm_u1p4_aplicacion_escolar/AvanceCurricular.kt
@@ -2,14 +2,42 @@ package com.example.dadm_u1p4_aplicacion_escolar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.dadm_u1p4_aplicacion_escolar.Adapters.RecyclerAdapterAvanceCurricular
+import com.example.dadm_u1p4_aplicacion_escolar.Models.Materia
 import com.example.dadm_u1p4_aplicacion_escolar.databinding.ActivityAvanceCurricularBinding
 
 class AvanceCurricular : AppCompatActivity() {
     private lateinit var binding: ActivityAvanceCurricularBinding
+    private lateinit var gridLayoutManager: GridLayoutManager
+    private lateinit var gridItemAdapter: RecyclerAdapterAvanceCurricular
+    private var models: MutableList<Materia> = mutableListOf<Materia>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAvanceCurricularBinding.inflate(layoutInflater)
         //setContentView(R.layout.activity_avance_curricular)
         setContentView(binding.root)
+
+        gridLayoutManager = GridLayoutManager(applicationContext, 6, LinearLayoutManager.VERTICAL, false)
+        binding.recyclerAvance.layoutManager = gridLayoutManager
+        binding.recyclerAvance.setHasFixedSize(true)
+        models = populateList()
+        gridItemAdapter = RecyclerAdapterAvanceCurricular(applicationContext, models)
+        binding.recyclerAvance.adapter = gridItemAdapter
+    }
+
+    private fun populateList(): MutableList<Materia>{
+        var items: MutableList<Materia> = mutableListOf<Materia>()
+
+        items.add(Materia("B1T1", "Calculo Diferencial", "73", "O1"))
+        items.add(Materia("B1T2", "Fundamentos de Programacion", "93", "O1"))
+        items.add(Materia("B1T3", "Matematicas Discretas", "70", "R1"))
+        items.add(Materia("B1T4", "Introduccion a las Tics", "83", "O1"))
+        items.add(Materia("B1T5", "Taller de Etica", "73", "O1"))
+        items.add(Materia("B1T6", "Fundamentos de Investigacion", "86", "R1"))
+
+        return items
     }
 }
--- DADM_U1P4_Aplicacion_Escolar/app/src/main/java/com/example/dadm_u1p4_aplicacion_escolar/Calificaciones.kt
+++ DADM_U1P4_Aplicacion_Escolar/app/src/main/java/com/example/dadm_u1p4_aplicacion_escolar/Calificaciones.kt
@@ -2,11 +2,18 @@ package com.example.dadm_u1p4_aplicacion_escolar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.dadm_u1p4_aplicacion_escolar.Adapters.RecyclerAdapterAvanceCurricular
+import com.example.dadm_u1p4_aplicacion_escolar.Models.Materia
 import com.example.dadm_u1p4_aplicacion_escolar.databinding.ActivityCalificacionesBinding
 
 class Calificaciones : AppCompatActivity() {
 
     private lateinit var binding: ActivityCalificacionesBinding
+    private lateinit var gridLayoutManager: GridLayoutManager
+    private lateinit var gridItemAdapter: RecyclerAdapterAvanceCurricular
+    private var models: MutableList<Materia> = mutableListOf<Materia>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +21,7 @@ class Calificaciones : AppCompatActivity() {
         binding = ActivityCalificacionesBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        gridLayoutManager = GridLayoutManager(applicationContext, 6, LinearLayoutManager.VERTICAL, false)
 
     }
 
