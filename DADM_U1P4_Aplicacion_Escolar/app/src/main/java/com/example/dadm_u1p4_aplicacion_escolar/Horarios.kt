074806c26b53a2040001d2f0f9585ce0c178c9de
panchis7u7 <smadrigal.rod@gmail.com>
2021-04-05 01:24:08 -0500

    Horarios cargados directamente desde firebase.

--- DADM_U1P4_Aplicacion_Escolar/app/src/main/java/com/example/dadm_u1p4_aplicacion_escolar/Horarios.kt
+++ DADM_U1P4_Aplicacion_Escolar/app/src/main/java/com/example/dadm_u1p4_aplicacion_escolar/Horarios.kt
@@ -2,6 +2,7 @@ package com.example.dadm_u1p4_aplicacion_escolar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.dadm_u1p4_aplicacion_escolar.Adapters.RecyclerAdapterCalificaciones
@@ -9,17 +10,54 @@ import com.example.dadm_u1p4_aplicacion_escolar.Adapters.RecyclerAdapterHorarios
 import com.example.dadm_u1p4_aplicacion_escolar.Models.Materia
 import com.example.dadm_u1p4_aplicacion_escolar.Models.ReporteSemestral
 import com.example.dadm_u1p4_aplicacion_escolar.databinding.ActivityHorariosBinding
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.core.Repo
+import com.google.firebase.firestore.FirebaseFirestore
 
 class Horarios : AppCompatActivity() {
+
     private lateinit var binding: ActivityHorariosBinding
+    private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHorariosBinding.inflate(layoutInflater)
         //setContentView(R.layout.activity_horarios)
         setContentView(binding.root)
 
-        horariosRecycler(populateList())
+        auth = FirebaseAuth.getInstance()
+        var semana: MutableList<ReporteSemestral> = mutableListOf()
+
+        db = FirebaseFirestore.getInstance()
+        db.collection("alumnos/${auth.currentUser.uid}/cursando").document("horarios").get()
+            .addOnSuccessListener { document ->
+                if (document != null){
+                    var dias: List<String> = listOf("lunes", "martes"/*, "Miercoles", "Jueves", "Viernes"*/)
+                    var horarios: MutableList<Materia>
+                    for(dia in dias) {
+                        horarios = mutableListOf()
+                        var horario: List<HashMap<String,Any>> = (document.get(dia) as List<HashMap<String,Any>>)
+                        //Log.d("Prueba", "Data: $horario")
+                        horario.map {
+                            var mat = (it as HashMap<String, Any>)
+                            Log.d("prueba2", "Datos2: ${(mat)}")
+                            horarios.add(Materia(
+                                aula = (mat.get("aula") as String),
+                                clave = (mat.get("clave") as String),
+                                grupo = (mat.get("grupo") as String),
+                                horario = (mat.get("horario") as String),
+                                materia = (mat.get("materia") as String),
+                                profesor = (mat.get("profesor") as String),
+                            ))
+                        }
+                        semana.add(ReporteSemestral(dia, horarios))
+                        horariosRecycler(semana)
+                    }
+                } else {
+                    Log.d("Error", "Error: No such document")
+                }
+            }
     }
 
     private fun populateList(): MutableList<ReporteSemestral>{
