4fe9dde032649bb248d60b225a1303cc6a4e507b
panchis7u7 <smadrigal.rod@gmail.com>
2021-05-11 19:20:21 -0500

    File restructure.

--- SGE_Services_Springboot/SGE/src/main/kotlin/com/scholar/SGE/Controllers/UserController.kt
+++ SGE_Services_Springboot/SGE/src/main/kotlin/com/scholar/SGE/Controllers/UserController.kt
@@ -1,8 +1,8 @@
 package com.scholar.SGE.Controllers
 
-import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("hola")
