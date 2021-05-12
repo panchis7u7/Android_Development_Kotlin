9391244b203402b0ee346128c74d6641236b3a87
panchis7u7 <smadrigal.rod@gmail.com>
2021-05-11 20:22:34 -0500

    Generated all file templates for upload.

--- DADM_U1P4_Aplicacion_Escolar/FirebaseLoader/main.py
+++ DADM_U1P4_Aplicacion_Escolar/FirebaseLoader/main.py
@@ -2,6 +2,7 @@
 # Libraries.
 
 import json
+import os
 import firebase_admin
 from firebase_admin import credentials, firestore
 
@@ -22,16 +23,25 @@ def load_json(file_path):
     with open(file_path, 'r') as f:
         return json.load(f)
 
+# Create all school subjects in a semester directory.
+def populate_data_template(semestres, materias):
+    for x in range(1, semestres+1):
+        dir_path = DATA_PATH + '/semestre' + str(x) 
+        os.mkdir(dir_path)
+        for y in range(1, materias+1):
+            open(dir_path + '/' + 'B'+str(x)+'T'+str(y)+".json", 'w+')
+
 ###################################################################################
 
 def main():
-    cred = credentials.Certificate(CERTIFICATE)
-    firebase_admin.initialize_app(cred)
-    store = firestore.client()
-    doc_ref = store.collection(u'Prueba')
-    docs = doc_ref.get()
-    item_data = load_json(DATA_PATH + '/' +"B1T1.json")
-    doc_ref.add(item_data)
+    #populate_data_template(9, 6)
+    #cred = credentials.Certificate(CERTIFICATE)
+    #firebase_admin.initialize_app(cred)
+    #store = firestore.client()
+    #doc_ref = store.collection(u'Prueba')
+    #docs = doc_ref.get()
+    #item_data = load_json(DATA_PATH + '/' +"B1T1.json")
+    #doc_ref.add(item_data)
 
 if __name__ == "__main__":
     main()
