4b35fe23260e0f27ec273b8e1b4d3f644be3006f
panchis7u7 <smadrigal.rod@gmail.com>
2021-05-11 19:52:09 -0500

    Firebase loader in python.

--- DADM_U1P4_Aplicacion_Escolar/FirebaseLoader/main.py
+++ DADM_U1P4_Aplicacion_Escolar/FirebaseLoader/main.py
@@ -11,7 +11,16 @@ from firebase_admin import credentials, firestore
 # Firebase API needs.
 
 CERTIFICATE = "./key.json"
-DATA_PATH = "data"
+DATA_PATH = "reticula"
+
+###################################################################################
+
+###################################################################################
+# Utility functions.
+
+def load_json(file_path):
+    with open(file_path, 'r') as f:
+        return json.load(f)
 
 ###################################################################################
 
@@ -21,6 +30,8 @@ def main():
     store = firestore.client()
     doc_ref = store.collection(u'Prueba')
     docs = doc_ref.get()
+    item_data = load_json(DATA_PATH + '/' +"B1T1.json")
+    doc_ref.add(item_data)
 
 if __name__ == "__main__":
     main()
