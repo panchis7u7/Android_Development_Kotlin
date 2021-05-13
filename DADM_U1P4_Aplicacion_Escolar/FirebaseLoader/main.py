09d26ccf72c57f5ef382d4f83acfe69984db3547
panchis7u7 <smadrigal.rod@gmail.com>
2021-05-12 19:42:52 -0500

    Upload each school subject to firebase.

--- DADM_U1P4_Aplicacion_Escolar/FirebaseLoader/main.py
+++ DADM_U1P4_Aplicacion_Escolar/FirebaseLoader/main.py
@@ -4,6 +4,7 @@
 import json
 import os
 import firebase_admin
+from os import listdir
 from firebase_admin import credentials, firestore, auth
 
 ###################################################################################
@@ -34,15 +35,20 @@ def populate_data_template(semestres, materias):
 ###################################################################################
 
 def main():
-    populate_data_template(9, 6)
+    #populate_data_template(9, 6)
     cred = credentials.Certificate(CERTIFICATE)
     app = firebase_admin.initialize_app(cred)
     store = firestore.client()
     userid = auth.get_user_by_email("smadrigal.rod@gmail.com", app).uid
     doc_ref = store.collection(u"alumnos/" + userid + "/materias")
     docs = doc_ref.get()
-    item_data = load_json(DATA_PATH + '/' +"B1T1.json")
-    doc_ref.add(item_data)
+    #item_data = load_json(DATA_PATH + '/' +"B1T1.json")
+    #doc_ref.add(item_data)
+    for dir in listdir(DATA_PATH):
+        for materia in listdir(DATA_PATH + '/' + dir):
+            data = load_json(DATA_PATH + '/' + dir + '/' + materia)
+            doc_ref.add(data)
+            print("File added: " + materia)
 
 if __name__ == "__main__":
     main()
