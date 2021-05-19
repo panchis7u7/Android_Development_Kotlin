c23759866bad721f00ee0d8beb517d99afc6d791
panchis7u7 <smadrigal.rod@gmail.com>
2021-05-18 19:58:26 -0500

    Cinema app.

---
+++ DADM_U2P2_Cine/app/src/main/java/com/example/dadm_u2p2_cine/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dadm_u2p2_cine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
