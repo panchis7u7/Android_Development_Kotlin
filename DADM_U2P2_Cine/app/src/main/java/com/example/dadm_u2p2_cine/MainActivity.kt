bfc5a295b8d26e825ccf88bdd0fcbb8dc7f7bad9
panchis7u7 <smadrigal.rod@gmail.com>
2021-05-20 03:30:31 -0500

    TODO: Add action bar back pressed support..

--- DADM_U2P2_Cine/app/src/main/java/com/example/dadm_u2p2_cine/MainActivity.kt
+++ DADM_U2P2_Cine/app/src/main/java/com/example/dadm_u2p2_cine/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.dadm_u2p2_cine
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.dadm_u2p2_cine.adapter.RecyclerPeliculasAdapter
@@ -20,8 +25,14 @@ class MainActivity : AppCompatActivity() {
         _binding = ActivityMainBinding.inflate(layoutInflater)
         //setContentView(R.layout.activity_main)
         setContentView(binding.root)
+        supportActionBar?.let {
+            it.title = "Cinepolis V2"
+            it.setDefaultDisplayHomeAsUpEnabled(true)
+            it.setDisplayHomeAsUpEnabled(true)
+        }
         //supportFragmentManager.findFragmentById(R.id.fragmentHome) as HomeFragment
 
+        setFullScreen()
         binding.bottomNavigationView.background = null
         binding.bottomAppBar.background = null
     }
@@ -29,5 +40,50 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+
+    override fun navigateUpTo(upIntent: Intent?): Boolean {
+        super.navigateUpTo(upIntent)
+        onBackPressed()
+        return true
+    }
+
+    private fun setFullScreen() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(true)
+            val controller = window.insetsController
+            if (controller != null) {
+                /*controller.hide(WindowInsets.Type.navigationBars()
+                        or WindowInsets.Type.captionBar())*/
+                controller.hide(WindowInsets.Type.captionBar())
+                //supportActionBar?.hide()
+                controller.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+        } else {
+            // All below using to hide navigation bar
+            val currentApiVersion = Build.VERSION.SDK_INT
+            val flags = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
+
+            // This work only for android 4.4+
+            if (currentApiVersion >= Build.VERSION_CODES.KITKAT) {
+                window.decorView.systemUiVisibility = flags
+                // Code below is to handle presses of Volume up or Volume down.
+                // Without this, after pressing volume buttons, the navigation bar will
+                // show up and won't hide
+                val decorView = window.decorView
+                decorView.setOnSystemUiVisibilityChangeListener { visibility: Int ->
+                    if (visibility and View.SYSTEM_UI_FLAG_FULLSCREEN == 0) {
+                        decorView.systemUiVisibility = flags
+                    }
+                }
+            }
+        }
     }
 }
