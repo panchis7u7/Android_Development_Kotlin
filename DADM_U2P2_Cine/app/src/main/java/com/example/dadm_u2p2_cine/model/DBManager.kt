63da55d8e728e794caf8842cd2eeb9446a8b92a5
panchis7u7 <smadrigal.rod@gmail.com>
2021-05-22 03:06:57 -0500

    Database schema android implementation.

--- DADM_U2P2_Cine/app/src/main/java/com/example/dadm_u2p2_cine/model/DBManager.kt
+++ DADM_U2P2_Cine/app/src/main/java/com/example/dadm_u2p2_cine/model/DBManager.kt
@@ -14,19 +14,25 @@ class DBManager(context: Context,
     override fun onCreate(db: SQLiteDatabase?) {
         val sql = """
             CREATE TABLE horarios (
-                id_horario INTEGER PRIMARY KEY NOT NULL, 
+                id_horario INTEGER PRIMARY KEY auto_increment, 
                 horario TEXT NOT NULL 
             );
             
             CREATE TABLE fechas (
-                id_fecha INTEGER PRIMARY KEY NOT NULL, 
-                fecha TEXT NOT NULL, 
+                id_fecha INTEGER PRIMARY KEY NOT NULL auto_increment, 
+                fecha TEXT NOT NULL
+            );
+            
+            CREATE TABLE fechashorarios (
+				id_fecha INTEGER NOT NULL, 
                 id_horario INTEGER NOT NULL, 
+                PRIMARY KEY (id_fecha, id_horario), 
+                FOREIGN KEY (id_fecha) REFERENCES fechas(id_fecha) ON UPDATE CASCADE ON DELETE CASCADE, 
                 FOREIGN KEY (id_horario) REFERENCES horarios(id_horario) ON UPDATE CASCADE ON DELETE CASCADE
             );
             
             CREATE TABLE peliculas (
-                id_pelicula INTEGER PRIMARY KEY NOT NULL, 
+                id_pelicula INTEGER PRIMARY KEY auto_increment, 
                 titulo TEXT NOT NULL, 
                 imagen TEXT NOT NULL, 
                 cover TEXT NOT NULL, 
@@ -46,7 +52,7 @@ class DBManager(context: Context,
             );
             
             CREATE TABLE compras (
-                id_compra INTEGER PRIMARY KEY NOT NULL,
+                id_compra INTEGER PRIMARY KEY auto_increment,
                 total REAL NOT NULL,
                 noAsientos INTEGER,
                 asientos TEXT,
@@ -55,18 +61,37 @@ class DBManager(context: Context,
                 FOREIGN KEY (id_pelicula) REFERENCES peliculas(id_pelicula) ON UPDATE CASCADE ON DELETE CASCADE
             );
             
-            INSERT INTO horarios VALUES ("9:00")
-            INSERT INTO horarios VALUES ("10:00")
-            INSERT INTO horarios VALUES ("11:00")
-            INSERT INTO horarios VALUES ("12:00")
-            INSERT INTO horarios VALUES ("13:00")
-            INSERT INTO horarios VALUES ("14:00")
-            INSERT INTO horarios VALUES ("15:00")
-            INSERT INTO horarios VALUES ("16:00")
-            INSERT INTO horarios VALUES ("17:00")
-            INSERT INTO horarios VALUES ("18:00")
-            INSERT INTO horarios VALUES ("19:00")
-            INSERT INTO horarios VALUES ("20:00")
+            INSERT INTO horarios (horario) VALUES ('9:00');
+            INSERT INTO horarios (horario) VALUES ("10:00");
+            INSERT INTO horarios (horario) VALUES ("11:00");
+            INSERT INTO horarios (horario) VALUES ("12:00");
+            INSERT INTO horarios (horario) VALUES ("13:00");
+            INSERT INTO horarios (horario) VALUES ("14:00");
+            INSERT INTO horarios (horario) VALUES ("15:00");
+            INSERT INTO horarios (horario) VALUES ("16:00");
+            INSERT INTO horarios (horario) VALUES ("17:00");
+            INSERT INTO horarios (horario) VALUES ("18:00");
+            INSERT INTO horarios (horario) VALUES ("19:00");
+            INSERT INTO horarios (horario) VALUES ("20:00");
+            
+            INSERT INTO fechas (fecha) VALUES ('11 Marzo');
+            INSERT INTO fechas (fecha) VALUES ('12 Marzo');
+            INSERT INTO fechas (fecha) VALUES ('13 Marzo');
+            INSERT INTO fechas (fecha) VALUES ('14 Marzo');
+            INSERT INTO fechas (fecha) VALUES ('15 Marzo');
+            INSERT INTO fechas (fecha) VALUES ('16 Marzo');
+            INSERT INTO fechas (fecha) VALUES ('17 Marzo');
+            INSERT INTO fechas (fecha) VALUES ('18 Marzo');
+            
+            INSERT INTO fechashorarios VALUES (1,1);
+            INSERT INTO fechashorarios VALUES (1,2);
+            INSERT INTO fechashorarios VALUES (1,3);
+            INSERT INTO fechashorarios VALUES (2,5);
+            INSERT INTO fechashorarios VALUES (2,6);
+            INSERT INTO fechashorarios VALUES (2,1);
+            INSERT INTO fechashorarios VALUES (5,1);
+            INSERT INTO fechashorarios VALUES (5,2);
+            INSERT INTO fechashorarios VALUES (4,4);
             
             """.trimIndent()
 
