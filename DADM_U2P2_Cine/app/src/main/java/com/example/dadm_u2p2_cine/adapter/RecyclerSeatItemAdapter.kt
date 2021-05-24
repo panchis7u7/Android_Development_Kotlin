5d119cee29241ee10e1c0957fb0eeb7e3e38931a
panchis7u7 <smadrigal.rod@gmail.com>
2021-05-24 18:19:38 -0500

    Final touch.

--- DADM_U2P2_Cine/app/src/main/java/com/example/dadm_u2p2_cine/adapter/RecyclerSeatItemAdapter.kt
+++ DADM_U2P2_Cine/app/src/main/java/com/example/dadm_u2p2_cine/adapter/RecyclerSeatItemAdapter.kt
@@ -36,6 +36,7 @@ RecyclerView.Adapter<RecyclerSeatItemAdapter.ViewHolder>(){
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.buttonSeat.background.setTint(context.getColor(R.color.buttonCategory))
     }
 
     override fun getItemCount(): Int = seats.size
