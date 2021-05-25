6d3d69340d22d3dc4b912964fabca3e5470b6d88
panchis7u7 <smadrigal.rod@gmail.com>
2021-05-25 17:17:56 -0500

    Database restructure.

--- SGE_Services_Springboot/SGE/src/main/kotlin/com/scholar/SGE/dao/AlumnoRepository.kt
+++ SGE_Services_Springboot/SGE/src/main/kotlin/com/scholar/SGE/dao/AlumnoRepository.kt
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Repository
 import org.springframework.data.jpa.repository.JpaRepository
 import com.scholar.SGE.model.Alumno
 
-@Repository
+@Repository("postgres")
 interface AlumnoRepository: JpaRepository<Alumno, Long> {
 
 }
