c3a72a64cc9946ce41ab8bb464e56d28d2d0ab4c
panchis7u7 <smadrigal.rod@gmail.com>
2021-05-25 00:58:08 -0500

    Added dummy alumno for API testing.

--- SGE_Services_Springboot/SGE/src/main/kotlin/com/scholar/SGE/SgeApplication.kt
+++ SGE_Services_Springboot/SGE/src/main/kotlin/com/scholar/SGE/SgeApplication.kt
@@ -2,9 +2,35 @@ package com.scholar.SGE
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.CommandLineRunner
+import org.springframework.beans.factory.annotation.Autowired
+import com.scholar.SGE.dao.AlumnoRepository
+import com.scholar.SGE.model.Alumno
+import java.time.format.DateTimeFormatter
+import java.time.LocalDate
 
 @SpringBootApplication
-class SgeApplication
+class SgeApplication: CommandLineRunner {
+	
+	@Autowired
+	val alumnoRepository: AlumnoRepository? = null
+
+	override fun run(vararg args: String?){
+
+		val formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
+		val alumno = Alumno(
+			"568734", 
+			"sebastian.prueba@gmail.com", 
+			"MICA82378DS872",
+			"Carlos Sebastian Madrigal Rodriguez",
+			LocalDate.parse("29-05-1999", formatter),
+			"44968521",
+			'M',
+			"foto")
+
+		alumnoRepository!!.save(alumno)
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<SgeApplication>(*args)
--- SGE_Services_Springboot/SGE/src/main/kotlin/com/scholar/SGE/model/Alumno.kt
+++ SGE_Services_Springboot/SGE/src/main/kotlin/com/scholar/SGE/model/Alumno.kt
@@ -6,6 +6,7 @@ import javax.persistence.Id
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import org.hibernate.annotations.GeneratorType
+import java.time.LocalDate
 
 @Entity
 @Table(name = "alumnos")
@@ -15,7 +16,7 @@ data class Alumno(
     val correo: String? = "",
     val curp: String? = "",
     val nombre: String? = "",
-    val fecha_nacimiento: String? = "",
+    val fecha_nacimiento: LocalDate? = null,
     val telefono: String? = "",
     val sexo: Char? = Char.MIN_VALUE,
     val fotografia: String? = ""
