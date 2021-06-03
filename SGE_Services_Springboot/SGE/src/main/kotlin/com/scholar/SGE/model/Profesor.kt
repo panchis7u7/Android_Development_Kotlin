099dcfb789df0dadc644bbd919e51ccbc9b88a2b
panchis7u7 <smadrigal.rod@gmail.com>
2021-06-03 12:38:08 -0500

    Correctly mapped each professor with his/her groups.

--- SGE_Services_Springboot/SGE/src/main/kotlin/com/scholar/SGE/model/Profesor.kt
+++ SGE_Services_Springboot/SGE/src/main/kotlin/com/scholar/SGE/model/Profesor.kt
@@ -10,9 +10,7 @@ data class Profesor(
     @Id @GeneratedValue(strategy = GenerationType.AUTO) @Column(name = "id_profesor") @JsonProperty("id") var id: Long,
     @Column(name = "nombre") var nombre: String,
 
-    /*@ManyToMany(fetch = FetchType.EAGER, cascade = arrayOf(CascadeType.ALL))
-    @JoinTable(name = "asignaturas_profesores",
-    joinColumns = arrayOf(JoinColumn(name = "id_profesor")),
-    inverseJoinColumns = arrayOf(JoinColumn(name = "id_asignatura")))
-    var asignaturas: List<Asignatura> = mutableListOf()*/
+    @OneToMany(mappedBy = "profesor")
+    @JsonIgnoreProperties("profesor")
+    var grupos: List<Grupo>
 ) {}
